8162067c4b7aaadaf5fa5834c149eef6d526cae9
Ryan Webb <webbryan92@gmail.com>
2019-03-14 14:57:07 -0400

    downvote and fixed image link

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :captions do 
     member do
       get 'increment_vote'
+      get 'decrement_vote'
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
